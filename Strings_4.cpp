76814a0f8256f1129e9310cc289ba2e6e04438db
Ananya Swadia <ananya.swadia@gmail.com>
2025-08-01 17:28:11 +0530

    Add files via upload

---
+++ Strings_4.cpp
@@ -0,0 +1,37 @@
+//Ananya Swadia
+//24070123012
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int len = str.length();
+    bool isPalindrome = true;
+
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+        cout << "The string is a palindrome." << endl;
+    else
+        cout << "The string is not a palindrome." << endl;
+
+    return 0;
+}
+
+/*  OUTPUT 
+Enter a string: malayalam
+The string is a palindrome.
+
+Enter a string: morning
+The string is not a palindrome.
+*/
